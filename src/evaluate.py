d687871744b67ab68926d295e59b033d5fd1707e
Safina Khan <safinakh2000@gmail.com>
2026-03-30 12:53:37 -0400

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,1 +1,20 @@
-# evaluation script
+import yaml
+import joblib
+from preprocess import load_and_preprocess
+
+def load_config():
+    with open("config/config.yaml") as f:
+        return yaml.safe_load(f)
+
+def evaluate():
+    cfg = load_config()
+    X, y = load_and_preprocess()
+
+    print("✅ Loading model...")
+    model = joblib.load(cfg["paths"]["model_registry"])
+
+    accuracy = model.score(X, y)
+    print(f"✅ Full Dataset Accuracy: {accuracy:.4f}")
+
+if __name__ == "__main__":
+    evaluate()
